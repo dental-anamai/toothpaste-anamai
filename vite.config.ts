23213df38bb79be085a667c130b79722b363e55f
Napat Phobutdee <pbd.napat1111@gmail.com>
2024-08-27 16:27:07 +0700

    optimize font load & remove analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginRadar({
-    // Google Analytics tag injection
-    analytics: {
-      id: 'G-WFWMPTVXR5',
-    },
-  })],
+  plugins: [react()],
   base: '/toothpaste-anamai/',
 })
